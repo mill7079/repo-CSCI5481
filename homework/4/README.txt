d8b98a22a53a7ba7065f659998d95ea08e72a2a2
Allison Miller <mill7079@umn.edu>
2021-12-02 23:47:34 -0600

    optimize reverse_bwt - now precomputes lf mapping to avoid going through the sequence however   many times - works Very Very Well, runs in a few seconds as opposed to hours - also added README - still need all of the fm mapping that I don't understand, though...

---
+++ homework/4/README.txt
@@ -0,0 +1,23 @@
+Allison Miller
+mill7079
+Homework 4
+
+All functions listed in the homework writeup are contained in main.py.
+
+
+To run:
+
+python3 main.py
+
+
+The main method automatically runs the functions on the file names suggested in the homework writeup;
+    however, these files aren't included in the submission as per the instructions, so if you want to
+    run the code you'll need to place those files in the files directory (as all the paths look for files
+    in that directory), and if you want to test new files, you'll need to change the filenames in the code.
+
+Additionally, for ease of grading, I've left the source file out in the main project folder instead of storing
+    it in its own folder; the IDE I use starts looking for files in the main project directory anyway, so it
+    saves some hassle of fixing relative paths before submission.
+
+All files mentioned in the rubric are included and named appropriately; there's an additional file called
+    reversedSample1.fa that's the reverse_bwt of sample1.fa for testing purposes.
--- homework/4/main.py
+++ homework/4/main.py
@@ -1,19 +1,30 @@
 from _collections import deque
 
 # end of file character appended to every sequence
-eof = '$'
+eof = "$"
 
 
 # helper for reverseBwt
-def lf(index, bwt, first):
+# def lf(index, bwt, first):
+#     char = bwt[index]
+#     offset = 0
+#     for i in range(0, index):
+#         if bwt[i] == char:
+#             offset += 1
+#
+#     new_index = first.index(char) + offset
+#     return new_index
+
+
+# theoretically faster lookup
+def lf(index, bwt, offsets):
     char = bwt[index]
     offset = 0
     for i in range(0, index):
         if bwt[i] == char:
             offset += 1
 
-    new_index = first.index(char) + offset
-    return new_index
+    return offsets[char] + offset
 
 
 # learning from hw2 mistakes and using a deque
@@ -31,21 +42,93 @@ def bwt(seq):
     return bwtSeq
 
 
-def reverseBwt(bwtSeq):
+# def reverse_bwt(bwtSeq):
+#     # recreate first column of table
+#     first = sorted(bwtSeq)
+#     seq = bwtSeq[0]
+#     i = 0
+#
+#     offsets = dict()  # stores offset of each new character
+#     for j in range(0, len(first)):
+#         if first[j] not in offsets:
+#             offsets[first[j]] = j
+#
+#     while bwtSeq[i] != eof:
+#         # i = lf(i, bwtSeq, first)
+#         i = lf(i, bwtSeq, offsets)
+#         seq = bwtSeq[i] + seq
+#
+#     return seq[1:]
+
+def reverse_bwt(bwtSeq):
     # recreate first column of table
     first = sorted(bwtSeq)
     seq = bwtSeq[0]
     i = 0
 
+    # calculate offsets of new characters in first column
+    offsets = dict()  # stores offset of each new character
+    for j in range(0, len(first)):
+        if first[j] not in offsets:
+            offsets[first[j]] = j
+
+    # precompute lf-mapping to speed up calculations
+    # because trying to reverse bwtHomoSapiens takes AGES
+    # even with the theoretically faster lookup
+    lf = [0] * len(bwtSeq)
+    for j in range(0, len(bwtSeq)):
+        lf[j] = offsets[bwtSeq[j]]
+        offsets[bwtSeq[j]] += 1  # need to update offset value as you go to prevent re-searching the array
+
     while bwtSeq[i] != eof:
-        i = lf(i, bwtSeq, first)
+        # i = lf(i, bwtSeq, offsets)
+        i = lf[i]
         seq = bwtSeq[i] + seq
 
     return seq[1:]
 
 
 if __name__ == '__main__':
-    print(bwt('acaacg$'))
-    print(reverseBwt(bwt('acaacg$')))
+    # testing
+    # print(bwt('acaacg$'))
+    # print(reverse_bwt(bwt('acaacg$')))
     # print(bwt('abc$'))
-    # print(reverseBwt(bwt('abc$')))
+    # print(reverse_bwt(bwt('abc$')))
+
+
+    # first function: bwt
+    bwt_in = open("files/sample1.fa")
+    bwt_in.readline()
+    seq = bwt_in.read().replace('\n', '') + eof
+    temp_seq = bwt(seq)
+
+    bwt_out = open("files/bwtSample1.fa", 'w')
+    bwt_out.write(">sample1\n")
+
+    while len(temp_seq) > 0:
+        if len(temp_seq) < 70:
+            bwt_out.write(temp_seq + '\n')
+            temp_seq = ''
+        else:
+            bwt_out.write(temp_seq[0:70] + '\n')
+            temp_seq = temp_seq[70:]
+
+    bwt_out.close()
+
+
+    # second function: reverse_bwt
+    # rev_in = open("files/bwtSample1.fa")
+    rev_in = open("files/bwtHomoSapiens.fa")
+    rev_in.readline()
+    seq = rev_in.read().replace('\n', '')
+    rev_seq = reverse_bwt(seq)
+
+    # rev_out = open("files/reversedSample1.fa", "w")
+    rev_out = open("files/rBwtSample2.fa", "w")
+    rev_out.write(">sample2\n")
+
+    while len(rev_seq) > 0:
+        rev_out.write(rev_seq[0:70] + '\n')
+        rev_seq = rev_seq[70:]
+
+    rev_out.close()
